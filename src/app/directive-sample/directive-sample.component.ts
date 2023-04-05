b4e1a9f20ba76d73f79923ad91cbd1f28758fcc7
dany1468 <dany1468@gmail.com>
2023-04-05 17:46:25 +0900

    npx ng g c DirectiveSample --standalone

---
+++ src/app/directive-sample/directive-sample.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-directive-sample',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './directive-sample.component.html',
+  styleUrls: ['./directive-sample.component.scss']
+})
+export class DirectiveSampleComponent {
+
+}
