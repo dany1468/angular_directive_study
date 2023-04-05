fdccb82e6beee45519f1404c88646083f571d965
dany1468 <dany1468@gmail.com>
2023-04-05 10:47:57 +0900

    Change AppComponent to standalone component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideRouter} from '@angular/router';
 
-import { AppModule } from './app/app.module';
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([])
+  ]
+});
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
